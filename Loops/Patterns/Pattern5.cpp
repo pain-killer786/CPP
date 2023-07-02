dcc3e128ba4e20a90803ef9590d0f7083a443fd2
merikh786 <krishnendunandy43@gmail.com>
2023-07-03 00:59:34 +0530

    Done

---
+++ Loops/Patterns/Pattern5.cpp
@@ -0,0 +1,18 @@
+//Write a program to print inverted half pyramid using numbers
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i,j;
+    cout<<"Enter the number of steps";
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            cout<<i;
+        }
+        cout<<endl;
+    }
+}
