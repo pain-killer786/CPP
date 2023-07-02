b5ad5d4ae41378b2ab6fb9b937deafba00ae6b74
merikh786 <krishnendunandy43@gmail.com>
2023-07-03 00:02:05 +0530

    Done

--- Loops/Patterns/Pattern1.cpp
+++ Loops/Patterns/Pattern1.cpp
@@ -2,5 +2,17 @@
 using namespace std;
 int main()
 {
+    int i,j,count;
+    cout<<"Enter the number of steps";
+    cin>>count;
+    for (int i = 1; i < count; i++)
+    {
+        for(j=1;j<count+1;j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;        
+    }
+    return 0;
+}    
     
-}
