11f2fb0e6cd8c26690ab9a6f9d618a19dd55f868
pain-killer786 <krishnendunandy43@gmail.com>
2023-09-03 13:07:51 +0530

    Done

---
+++ Q by MNC/Q4.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int r,unit,n;
+    cin>>r>>unit>>n;
+    cout<<"Enter the number of rats"<<endl;
+    cout<<"Enter the amount of food each rat consumes"
+}
