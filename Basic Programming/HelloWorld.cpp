cc82ac616f44de1c6e75ab7e21b030e37415b457
merikh786 <krishnendunandy43@gmail.com>
2023-10-07 22:28:54 +0530

    Added Few Information

--- Basic Programming/HelloWorld.cpp
+++ Basic Programming/HelloWorld.cpp
@@ -1,7 +1,10 @@
+// This is a single line comment.
+/* This is a multi line comment*/
 #include<iostream>
 using namespace std;
 int main()
 {
+    std::cout<<"Hello World";  // Here :: is called scope resoulter.
     cout<<"Hello World"<<endl;
     return 0;
 }
