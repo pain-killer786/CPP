28e2a3863936b95697dccfd7db8913c33d9a8235
merikh786 <krishnendunandy43@gmail.com>
2023-10-08 20:43:54 +0530

    Done

---
+++ Basic Programming/Roundoff.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+    float n;
+    cout<<"Enter a number"<<endl;
+    cin>>n;
+    cout<<"Floor is : "<< floor(n)<<endl;
+    cout<<"Ceil is :"<< ceil(n)<<endl;
+
+    return 0;
+}
