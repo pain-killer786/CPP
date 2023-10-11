318748f2d6ebb3721caec95585cdded2ad3d4a27
merikh786 <krishnendunandy43@gmail.com>
2023-10-11 20:53:30 +0530

    Optimezed the code

--- Data Structure And Algorithms/Arrays/Sorting/BubbleSort.cpp
+++ Data Structure And Algorithms/Arrays/Sorting/BubbleSort.cpp
@@ -1,42 +1,43 @@
 // Repeatedly swap two adjacent elements if they are in Wrong Order.
 
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main()
-{
-    int size,i;
-    cout<<"Enter the size of the array"<<endl;
-    cin>>size;
+
+void BubbleSort(int arr[], int n) {
+    for (int count = 0; count < n - 1; count++) {
+        bool swapped = false;
+        for (int i = 0; i < n - count - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                swap(arr[i], arr[i + 1]);
+                swapped = true;
+            }
+        }
+        if (!swapped) {
+            // If no swaps were made in a pass, the array is already sorted.
+            break;
+        }
+    }
+}
+
+int main() {
+    int size;
+    cout << "Enter the size of the array: ";
+    cin >> size;
 
     int arr[size];
 
-    for(i=0;i<size;i++)
-    {
-        cout<<"Enter a number"<<endl;
-        cin>>arr[i];
+    for (int i = 0; i < size; i++) {
+        cout << "Enter a number: ";
+        cin >> arr[i];
     }
 
-    int count=1;
+    BubbleSort(arr, size);
 
-    while(count<size-1)
-    {
-        for(int i=0;i<size-count;i++)
-        {
-            if(arr[i]>arr[i+1])
-            {
-                int temp=arr[i];
-                arr[i]=arr[i+1];
-                arr[i+1]=temp;
-            }
-        }
-        count++;
+    cout << "Sorted array: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
     }
-
-    for(int i=0;i<size;i++)
-    {
-        cout<<arr[i]<<" ";
-    }
-    cout<<endl;
+    cout << endl;
 
     return 0;
 }
