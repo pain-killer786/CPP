8b22d1978cf4d493b30ce39285980f185a6d8c63
merikh786 <krishnendunandy43@gmail.com>
2023-10-12 11:23:41 +0530

    Optimized the code

--- Data Structure And Algorithms/Arrays/Sorting/InsertionSort.cpp
+++ Data Structure And Algorithms/Arrays/Sorting/InsertionSort.cpp
@@ -1,38 +1,46 @@
 //Insert an element from unsorted array to its correct position in sorted array
 
 #include<iostream>
+#include<vector>
 using namespace std;
-int main()
-{
-    int n,i;
-    cout<<"Enter the size of the array"<<endl;
-    cin>>n;
 
-    int arr[n];
+void insertionSort(vector<int>& arr) {
+    int n = arr.size();
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
 
-    for(i=0;i<n;i++)
-    {
-        cout<<"Enter a number"<<endl;
-        cin>>arr[i];
-    }
-    
-    for(int i=1;i<n;i++)
-    {
-        int current=arr[i];
-        int j=i-1;
-        while(arr[j]>current && j>=0)
-        {
-            arr[j+1]=arr[j];
+        /* Move elements of arr[0..i-1] that are greater than key to one position ahead of their current position*/
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
             j--;
         }
-        arr[j+1]=current;
+        arr[j + 1] = key;
+    }
+}
+
+int main() 
+{
+    vector<int> arr;
+    int n, num;
+
+    cout<<"Enter the number of elements: ";
+    cin>>n;
+
+    cout<<"Enter the elements:\n";
+    for(int i = 0; i < n; i++)
+    {
+        cin>>num;
+        arr.push_back(num);
     }
 
-    for(int i=0;i<n;i++)
-    {
-        cout<<arr[i]<<" ";
+    insertionSort(arr);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
     }
-    cout<<endl;
+    cout <<endl;
 
     return 0;
 }
