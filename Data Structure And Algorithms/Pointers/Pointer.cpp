a57c84f4f344cea70a251ec4678ca32664bf7ae0
Merikh <krishnendunandy43@gmail.com>
2023-12-28 14:08:26 +0530

    Added Comments

--- Data Structure And Algorithms/Pointers/Pointer.cpp
+++ Data Structure And Algorithms/Pointers/Pointer.cpp
@@ -1,13 +1,13 @@
-#include<bits/stdc++.h>
+#include<bits/stdc++.h> //this includes all the libraries in cpp
 using namespace std;
 int main()
 {
-    int a=10;
-    int *aptr;
-    aptr= &a;
-    int **aqtr=&aptr;
-    cout<<aptr<<endl;
-    cout<<*aqtr<<endl;
+    int a=10; //variable named a stores the value a;
+    int *aptr; // Declared a Pointer named aptr
+    aptr= &a; // aptr stores the address of a
+    int **aqtr=&aptr; //Declared another pointer aqtr which points to the aptr pointer
+    cout<<aptr<<endl; //Prints the address of a 
+    cout<<*aqtr<<endl; //Prints the value of aptr i.e. the address of a 
 
     return 0;
 }
