0e01e29c9daf31df0c1c9d72382ec113974ef4a3
Krishnendu Nandy <krishnendunandy43@gmail.com>
2024-03-10 02:20:18 +0530

    Done

---
+++ Basic Programming/String.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s="Krishnendu Nandy";
+    int length= s.size();
+    cout<<s<<endl;
+    cout<<length<<endl;
+    return 0;
+}
