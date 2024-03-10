c147c340d98afddeaef7b6d9f961d2c71ab52d21
Krishnendu Nandy <krishnendunandy43@gmail.com>
2024-03-10 14:19:20 +0530

    Done

---
+++ Loops/Patterns/Pattern11.cpp
@@ -0,0 +1,44 @@
+// Write a program to print diamond pattern
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i,j,k;
+    printf("Enter a number");
+    cin>>n;
+
+    for (int i = 0; i < n; i++) 
+    {
+      for (int j = 0; j < n - i - 1;j++)
+      {
+          cout<<" ";          
+      }
+      for (int k=0;k<2*i+1;k++)
+      {
+          cout<<"*";
+      }
+      for (int j=0;j<n-i-1;j++)
+      {
+          cout<<" ";
+      }
+      cout<<endl;   
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+      for(int j=0;j<i;j++)
+       {
+           cout<<" ";
+       }
+       for(int k=0;k<2*n-(2*i+1);k++)
+       {
+           cout<<"*";
+       }
+       for(int j=0;j<i;j++)
+       {
+           cout<<" ";
+       }        
+       cout<<endl;
+    }
+}
