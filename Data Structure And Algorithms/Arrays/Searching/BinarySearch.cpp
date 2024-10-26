0dd80e102279307d69a364f4369a280417e0b92b
white knight <krishnendunandy43@gmail.com>
2024-10-26 12:52:21 +0530

    Updated the code

--- Data Structure And Algorithms/Arrays/Searching/BinarySearch.cpp
+++ Data Structure And Algorithms/Arrays/Searching/BinarySearch.cpp
@@ -6,27 +6,26 @@ using namespace std;
 int BinarySearch(int arr[],int n,int key)
 {
     int start=0;
-    int end=n;
+    int end=n-1;
+    int mid= start+(end-start)/2;
     while(start<=end)
     {
-        int mid=(start+end)/2;
-
         if(arr[mid]==key)
         {
             return mid;
         }
         else if (arr[mid]>key)
         {
-            start=mid-1;
+            end=mid-1;
         }
         else
         {
             start=mid+1;
         }
+        mid= start+(end-start)/2;
     }
     return -1;
 }
-
 
 
 
@@ -46,7 +45,7 @@ int main()
         cin>>arr[i];
     }    
 
-    cout<<"Element Found At"<<BinarySearch(arr,size,key)<<endl;
+    cout<<"Element Found At "<<BinarySearch(arr,size,key)<<endl;
 
     return 0;
 }
