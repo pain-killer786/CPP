9350bcd97ac200411ee86a1996ba1bc137a5adff
white knight <krishnendunandy43@gmail.com>
2024-12-08 20:30:20 +0530

    Updated Code

--- Data Structure And Algorithms/Arrays/Sorting/SelectionSort.cpp
+++ Data Structure And Algorithms/Arrays/Sorting/SelectionSort.cpp
@@ -1,38 +1,55 @@
 // Find the minimum element in unsorted array and swap it with element at the beginning
 
-#include<iostream>
+#include <iostream>
 using namespace std;
 
+// Function to perform Selection Sort
+void SelectionSort(int arr[], int n)
+{
+    for(int i=0;i<n-1;i++)
+    {
+        int minIndex =i;
+        
+        // Find the index of the smallest element in the unsorted position
+        for(int j=i+1;j<n;j++)
+        {
+            if(arr[j]<arr[minIndex])
+            {
+                minIndex=j;
+            }
+        }
+
+        //Swap the smallest element eith the current element at position 'i'
+        swap(arr[minIndex], arr[i]);
+    }
+}
+
+//Main Function
 int main()
 {
-    int size,i;
+    int size;
+
+    //Input the size of the array
     cout<<"Enter the size of the array"<<endl;
     cin>>size;
-
+    
     int arr[size];
 
-    for(i=0;i<size;i++)
+    //Input array elements
+    for(int i=0;i<size;i++)
     {
         cout<<"Enter a number"<<endl;
         cin>>arr[i];
     }
 
-    for(i=0;i<size-1;i++)
-    {
-        for(int j=i+1;j<size;j++)
-        {
-            if(arr[j]<arr[i])
-            {
-                int temp=arr[j];
-                arr[j]=arr[i];
-                arr[i]=temp;
-            }
-        }
-    }
+    //Sort the array using Selection Sort
+    SelectionSort(arr, size);
 
-    for(int i=0;i<size;i++)
+    //Print the Sorted array
+    cout<<"The Sorted array is: ";
+    for (int i=0;i<size;i++)
     {
-        cout<<arr[i]<<" ";
+        cout<<arr[i]<< " ";
     }
     cout<<endl;
 
