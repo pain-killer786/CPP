5c013adc66bc84902ccbb7453d61be4377d14534
white knight <krishnendunandy43@gmail.com>
2024-12-09 12:20:23 +0530

    Updated Code

--- Data Structure And Algorithms/Arrays/Sorting/InsertionSort.cpp
+++ Data Structure And Algorithms/Arrays/Sorting/InsertionSort.cpp
@@ -1,46 +1,53 @@
 //Insert an element from unsorted array to its correct position in sorted array
 
-#include<iostream>
-#include<vector>
+#include<bits/stdc++.h>
 using namespace std;
 
-void insertionSort(vector<int>& arr) {
-    int n = arr.size();
-    for (int i = 1; i < n; i++) {
-        int key = arr[i];
-        int j = i - 1;
+void InsertionSort(int arr[], int n)
+{
+    //Start from the second element (index 1) because a single element is always "sorted"
+    for (int i=1;i<n;i++)
+    {
+        int key =arr[i]; // The element we want to place in the correct position
+        int j=i-1;
 
-        /* Move elements of arr[0..i-1] that are greater than key to one position ahead of their current position*/
-        while (j >= 0 && arr[j] > key) {
-            arr[j + 1] = arr[j];
+        //Move elements of arr[0..i-1] that are greater than 'key' to one position
+        while (j>=0 && arr[j]>key)
+        {
+            arr[j+1]=arr[j];
             j--;
         }
-        arr[j + 1] = key;
+
+        //Place 'key in its correct position
+        arr[j+1] = key;
     }
 }
 
-int main() 
+int main()
 {
-    vector<int> arr;
-    int n, num;
+    int size;
 
-    cout<<"Enter the number of elements: ";
-    cin>>n;
+    cout<<"Enter the size of the array"<<endl;
+    cin>>size;
 
-    cout<<"Enter the elements:\n";
-    for(int i = 0; i < n; i++)
+    int arr[size];
+
+    for(int i=0;i<size;i++)
     {
-        cin>>num;
-        arr.push_back(num);
+        cout<<"Enter the number"<<endl;
+        cin>>arr[i];
     }
 
-    insertionSort(arr);
+    InsertionSort(arr,size);
 
-    cout << "Sorted array: ";
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
+
+    //Print the Sorted array
+    cout<<"The Sorted array is: ";
+    for (int i=0;i<size;i++)
+    {
+        cout<<arr[i]<< " ";
     }
-    cout <<endl;
+    cout<<endl;
 
     return 0;
 }
