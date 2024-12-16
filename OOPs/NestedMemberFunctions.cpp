f59a94c599be6548613e7b085a4aa629170d60ac
white knight <krishnendunandy43@gmail.com>
2024-12-16 14:52:45 +0530

    Added a new Program

---
+++ OOPs/NestedMemberFunctions.cpp
@@ -0,0 +1,90 @@
+// OOPs - Classes and objects
+
+// C++ --> initially called --> C with classes by stroustroup
+// class --> extension of structures (in C)
+// structures had limitations
+//      - members are public
+//      - No methods
+// classes --> structures + more
+// classes --> can have methods and properties
+// classes --> can make few members as private & few as public
+// structures in C++ are typedefed
+// you can declare objects along with the class declarion like this:
+/* class Employee{
+            // Class definition
+        } harry, rohan, lovish; */
+// harry.salary = 8 makes no sense if salary is private
+
+// Nesting of member functions
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class binary
+{
+private:
+    string s;           // A private string member to store the binary number
+    void chk_bin(void); // A private member function to check if the string is a valid binary number.
+
+public:
+    void read(void);            // Public function to read the input from the user
+    void ones_compliment(void); // Public function to compute the one's complement of the binary string
+    void display(void);         // Public function to display the binary string
+};
+
+void binary::read(void)
+{
+    cout << "Enter a binary number" << endl;
+    cin >> s;
+}
+
+void binary::chk_bin(void)
+{
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s.at(i) != '0' && s.at(i) != '1')
+        {
+            cout << "Incorrect binary format" << endl;
+            exit(0);
+        }
+    }
+}
+
+void binary::ones_compliment(void)
+{
+    chk_bin();
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s.at(i) == '0')
+        {
+            s.at(i) = '1';
+        }
+        else
+        {
+            s.at(i) = '0';
+        }
+    }
+}
+
+void binary::display(void)
+{
+    cout << "Displaying your binary number" << endl;
+    for (int i = 0; i < s.length(); i++)
+    {
+        cout << s.at(i);
+    }
+    cout << endl;
+}
+
+int main()
+{
+    binary b;
+    b.read();
+    // b.chk_bin();
+    b.display();
+    b.ones_compliment();
+    b.display();
+
+    return 0;
+}
